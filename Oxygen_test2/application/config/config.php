b34f19d941c04e1ba39f48ab29fae145d0705793
arian.liu@gmail.com <arian.liu@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2011-09-01 06:01:08 +0000

    I have updated the base URL of the project under config.php

--- Oxygen_test2/application/config/config.php
+++ Oxygen_test2/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/Oxygen_test2/';
+$config['base_url']	= 'http://localhost:9081';
 
 /*
 |--------------------------------------------------------------------------
