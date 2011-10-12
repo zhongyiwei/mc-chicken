06e95eeeecc90c2bb7420a402a4687cdefea3376
sippoujulian@gmail.com <sippoujulian@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2011-10-12 06:20:10 +0000

    Continue Update for the Thinkquest 2

---
+++ Oxygen_test2/application/views/register/error_email.php
@@ -0,0 +1,9 @@
+<?php $this->load->view('includes/header_general');?>
+<?php $this->load->view('includes/banner_general');?>
+
+<h1>&otimes;Error&otimes;</h1>
+
+<p>The email you entered has been registered, Please use an other email address.Please wait for a while, system will link to register page.</p>
+
+
+<?php $this->load->view('includes/footer_general'); ?>
---
+++ Oxygen_test2/application/views/register/error_password.php
@@ -0,0 +1,8 @@
+<?php $this->load->view('includes/header_general');?>
+<?php $this->load->view('includes/banner_general');?>
+
+<h1>&otimes;Error&otimes;</h1>
+
+<p>Your Old Password is not correct, Please try <a href="<?php echo base_url();?>index.php/home/change_password/">again</a></p>
+
+<?php $this->load->view('includes/footer_general'); ?>
