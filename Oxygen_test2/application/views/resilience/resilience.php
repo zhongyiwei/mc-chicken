58908b1e45a23aa174abdb6655682f6c8c03c6d8
teowenjie@yahoo.com.sg <teowenjie@yahoo.com.sg@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-08 13:53:33 +0000

    edited resilience_test link. (Not working now as settings is for sit server currently)

--- Oxygen_test2/application/views/resilience/resilience.php
+++ Oxygen_test2/application/views/resilience/resilience.php
@@ -1,5 +1,12 @@
 <link href="<?php echo base_url();?>CSS/style_subpage_main.css" rel="stylesheet" type="text/css" media="screen" />
 <div id="page">
+         <div id="sub-nav">
+        <ul>
+            <li><a href="<?php echo base_url(); ?>index.php/home/resilience_test/gameproto.html" >Resilience Test</a></li>
+
+
+        </ul>
+         </div>
     <div id="content_sub">
         <div class="post">
             <h2 class="title">Resilience Module</h2>
