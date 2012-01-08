e93903c61a73eaf9f584dcd3b265dea079e9d6ae
teowenjie@yahoo.com.sg <teowenjie@yahoo.com.sg@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-08 04:09:01 +0000

    Updated resilience and values page (changed the footer, changed design of values page to improve visibility & editing of the error found when clicked more than 4 box the newly clicked is still in 'checked' state

--- Oxygen_test2/application/views/resilience/subpage_resilience.php
+++ Oxygen_test2/application/views/resilience/subpage_resilience.php
@@ -1,9 +1,7 @@
 <?php $this->load->view('includes/header_general');?>
 
-<?php $this->load->view('includes/banner_general');?>
-
 <?php $this->load->view($main);?>
 
 <?php $this->load->view($nav);?>
 
-<?php $this->load->view('includes/footer_general');?>
+<?php $this->load->view('subpage/sub_footer');?>
