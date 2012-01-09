cfea7bdbec8a0058ac10c48a6cea1453bd9c091c
teowenjie@yahoo.com.sg <teowenjie@yahoo.com.sg@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-09 15:25:54 +0000

    flowchart working(not integrated with logged in yet)

--- trunk/Oxygen_test2/application/views/next/get_flow.php
+++ trunk/Oxygen_test2/application/views/next/get_flow.php
@@ -1,13 +1,111 @@
-<?php $logged=true;
-if($logged==true){
+<?php $logged=$_POST['logged'];
+
+if($logged=='yes'){
+$session_id='1';
+
+$checkMission = $this->db->query("SELECT * FROM mission WHERE seeker_id='$session_id'");
+$checkValue = $this->db->query("SELECT * FROM seeker_value WHERE seeker_id='$session_id'");
+$checkGoal = $this->db->query("SELECT * FROM goal WHERE seeker_id='$session_id'");
+$checkActivityStatus=false;
+foreach ($checkGoal->result() as $row)
+{
+  $seeker_goal_id=$row->seeker_goal_id;
+  $checkActivity = $this->db->query("SELECT * FROM activity WHERE seeker_goal_id='$seeker_goal_id'");
+	if($checkActivity->num_rows()>0){
+	$checkActivityStatus=true;
+	break;
+	}
+}
+$checkResilience = $this->db->query("SELECT * FROM test_result WHERE seeker_id='$session_id'");
+$checkCOA = $this->db->query("SELECT * FROM coat_of_arm WHERE seeker_id='$session_id'");
+
+
+
 ?>
-<a href="<?php echo base_url();?>index.php/home/portfolio_coa_motto/#MS"><img width="100px" height="100px" src="<?php echo base_url();?>web_images/flowchart/Mission - Red (PNG).png"></img></a>
-<a href="<?php echo base_url();?>index.php/home/determineValue/"><img width="100px" height="100px" src="<?php echo base_url();?>web_images/flowchart/Value-Red(PNG).png"></a>
-<a href="<?php echo base_url();?>index.php/home/goal/"><img wwidth="100px" height="100px"  src="<?php echo base_url();?>web_images/flowchart/Goals-Red(PNG).png"></a>
-<a href="<?php echo base_url();?>index.php/home/activity_page/"><img width="100px" height="100px"  src="<?php echo base_url();?>web_images/flowchart/Activity-Red(PNG).png"></a>
-<a href="<?php echo base_url();?>index.php/home/resilience/"><img width="100px" height="100px"  src="<?php echo base_url();?>web_images/flowchart/Resilience-Red(PNG).png"></a>
-                        <a href="<?php echo base_url();?>index.php/home/portfolio_coa_motto/#MS"><img width="100px" height="100px" src="<?php echo base_url();?>web_images/flowchart/Coat of Arms-Red(PNG).png"></a>
-                        <a href="<?php echo base_url();?>index.php/home/portfolio_coa_motto/#MS"><img width="100px" height="100px"  src="<?php echo base_url();?>web_images/flowchart/Portfolio-Red(PNG).png"></a>
+
+<a href="<?php echo base_url();?>index.php/home/portfolio_coa_motto/#MS">
+<?php if($checkMission->num_rows() >0){//Has mission statement set ?>
+<img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Mission-Green.png"></img></a>
+<?php } else{ ?>
+<img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Mission-Blue (PNG).png"></img></a>
+<?php } ?>
+<img width="60px" height="60px" src="<?php echo base_url();?>web_images/flowchart/arrow.png"></img>
+<?php if($checkValue->num_rows() >0){ ?>
+<a href="<?php echo base_url();?>index.php/home/determineValue/"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Value-Green(PNG).png"></a>
+<?php } 
+else if($checkMission->num_rows() >0){ ?>
+<a href="<?php echo base_url();?>index.php/home/determineValue/"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Value-Blue (PNG).png"></a> <?php
+} else { ?>
+<a href="<?php echo base_url();?>index.php/home/determineValue/"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Value-Red(PNG).png"></a>
+<?php } ?>
+
+<img width="60px" height="60px" src="<?php echo base_url();?>web_images/flowchart/arrow.png"></img>
+
+
+<?php if($checkGoal->num_rows() >0){ ?>
+<a href="<?php echo base_url();?>index.php/home/goal/"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Goals-Green(PNG).png"></a>
+<?php } 
+else if($checkValue->num_rows() >0){ ?>
+<a href="<?php echo base_url();?>index.php/home/goal/"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Goals-Blue(PNG).png"></a> <?php
+} else { ?>
+<a href="<?php echo base_url();?>index.php/home/goal/"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Goals-Red(PNG).png"></a>
+<?php } ?>
+
+
+
+<img width="60px" height="60px" src="<?php echo base_url();?>web_images/flowchart/arrow.png"></img>
+
+<?php if($checkActivityStatus==true){//means there is activity for this user ?>
+<a href="<?php echo base_url();?>index.php/home/activity_page/"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Activity-Green(PNG).png"></a>
+<?php } 
+else if($checkGoal->num_rows() >0){ ?>
+<a href="<?php echo base_url();?>index.php/home/activity_page/"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Activity-Blue(PNG).png"></a> <?php
+} else { ?>
+<a href="<?php echo base_url();?>index.php/home/activity_page/"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Activity-Red(PNG).png"></a>
+<?php } ?>
+
+
+<img width="60px" height="60px" src="<?php echo base_url();?>web_images/flowchart/arrow.png"></img>
+
+
+
+<?php if($checkResilience->num_rows()>0){?>
+<a href="<?php echo base_url();?>index.php/home/resilience/"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Resilience-Green(PNG).png"></a>
+<?php } 
+else if($checkActivityStatus==true){ ?>
+<a href="<?php echo base_url();?>index.php/home/resilience/"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Resilience-Blue(PNG).png"></a> <?php
+} else { ?>
+<a href="<?php echo base_url();?>index.php/home/resilience/"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Resilience-Red(PNG).png"></a>
+<?php } ?>
+
+
+<img width="60px" height="60px" src="<?php echo base_url();?>web_images/flowchart/arrow.png"></img>
+
+<?php if($checkCOA->num_rows()>0){?>
+<a href="<?php echo base_url();?>index.php/home/portfolio_coa_motto/#MS"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Coat of Arms-Green(PNG).png"></a>
+<?php } 
+else if($checkResilience->num_rows()>0){ ?>
+<a href="<?php echo base_url();?>index.php/home/portfolio_coa_motto/#MS"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Coat of Arms-Blue(PNG).png"></a> <?php
+} else { ?>
+<a href="<?php echo base_url();?>index.php/home/portfolio_coa_motto/#MS"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Coat of Arms-Red(PNG).png"></a>
+<?php } ?>
+
+						<img width="60px" height="60px" src="<?php echo base_url();?>web_images/flowchart/arrow.png"></img>
+                        <a href="<?php echo base_url();?>index.php/home/portfolio_coa_motto/#MS"><img width=width="80px" height="80px"  src="<?php echo base_url();?>web_images/flowchart/Portfolio-Red(PNG).png"></a>
+					
+		<!--		<img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Mission - Red (PNG).png"></img></a>		
+<img width="60px" height="60px" src="<?php echo base_url();?>web_images/flowchart/arrow.png"></img>
+<a href="<?php echo base_url();?>index.php/home/determineValue/"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Value-Red(PNG).png"></a>
+<img width="60px" height="60px" src="<?php echo base_url();?>web_images/flowchart/arrow.png"></img>
+<a href="<?php echo base_url();?>index.php/home/goal/"><img width="80px" height="80px"  src="<?php echo base_url();?>web_images/flowchart/Goals-Red(PNG).png"></a>
+<img width="60px" height="60px" src="<?php echo base_url();?>web_images/flowchart/arrow.png"></img>
+<a href="<?php echo base_url();?>index.php/home/activity_page/"><img width="80px" height="80px"  src="<?php echo base_url();?>web_images/flowchart/Activity-Red(PNG).png"></a>
+<img width="60px" height="60px" src="<?php echo base_url();?>web_images/flowchart/arrow.png"></img>
+<a href="<?php echo base_url();?>index.php/home/resilience/"><img width="80px" height="80px"  src="<?php echo base_url();?>web_images/flowchart/Resilience-Red(PNG).png"></a>
+<img width="60px" height="60px" src="<?php echo base_url();?>web_images/flowchart/arrow.png"></img>
+                        <a href="<?php echo base_url();?>index.php/home/portfolio_coa_motto/#MS"><img width="80px" height="80px" src="<?php echo base_url();?>web_images/flowchart/Coat of Arms-Red(PNG).png"></a>
+						<img width="60px" height="60px" src="<?php echo base_url();?>web_images/flowchart/arrow.png"></img>
+                        <a href="<?php echo base_url();?>index.php/home/portfolio_coa_motto/#MS"><img width=width="80px" height="80px"  src="<?php echo base_url();?>web_images/flowchart/Portfolio-Red(PNG).png"></a> -->
 <?php }
 else{
 echo "<p>You are not logged in!</p>";
