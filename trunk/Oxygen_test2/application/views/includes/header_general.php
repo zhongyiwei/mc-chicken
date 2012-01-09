866c482bb57a8e15de9c12c988604de83d713a3b
teowenjie@yahoo.com.sg <teowenjie@yahoo.com.sg@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-09 07:41:29 +0000

    updated few files for workflow(Not working now)

--- trunk/Oxygen_test2/application/views/includes/header_general.php
+++ trunk/Oxygen_test2/application/views/includes/header_general.php
@@ -162,7 +162,60 @@ and open the template in the editor.
 
 
  </script>
+ 
+ 
 
+ 
+ 
+              <script language="JavaScript" type="text/javascript">//WenJie function for what's next
+            function loadFlow(){
+<?php  if (isset($is_logged_in) && ($is_logged_in == 'true')) { ?>
+var loggedin=true;
+<?php } else {?>
+var loggedin=false; <?php } ?>
+
+    
+
+                // Create our XMLHttpRequest object
+                var hr = new XMLHttpRequest();
+                // Create some variables we need to send to our PHP file
+                var url = '<?php echo base_url()."home/";?>'+"getflow";
+alert(url);
+                var vars = "logged="+loggedin;
+                hr.open("POST", url, true);
+                // Set content type header information for sending url encoded variables in the request
+                hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                // Access the onreadystatechange event for the XMLHttpRequest object
+                hr.onreadystatechange = function() {
+                    if(hr.readyState == 4 && hr.status == 200) {
+                        var return_data = hr.responseText;
+			
+                        document.getElementById("dialog").innerHTML = "BOO";
+                    }
+                }
+                // Send the data to PHP now... and wait for response to update the status div
+                hr.send(vars); // Actually execute the request
+                document.getElementById("dialog").innerHTML = "processing...";
+                }
+
+        </script>
+
+<script> //Dialog box for what's nex
+	// increase the default animation speed to exaggerate the effect
+	$.fx.speeds._default = 1000;
+	$(function() {
+		$( "#dialog" ).dialog({
+			autoOpen: false,
+			show: "blind",
+			hide: "explode"
+		});
+
+		$( "#opener" ).click(function() {
+			$( "#dialog" ).dialog( "open" );
+			return false;
+		});
+	});
+	</script>
     <script language="javascript" type="text/javascript">
 	//tabs---wenjie
 	            $(function() {
---
+++ trunk/Oxygen_test2/application/views/next/get_flow.php
@@ -0,0 +1,8 @@
+<?php $logged=false;
+if($logged==true){
+echo "<p>You are logged in!</p>";
+}
+else{
+echo "<p>You are not logged in!</p>";
+}
+?>
