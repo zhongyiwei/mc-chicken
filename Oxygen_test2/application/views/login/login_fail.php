166a4fbb6c066b0d129c92a05e516c09db20303f
tom.caibowen@gmail.com <tom.caibowen@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-10 12:44:22 +0000

    login done

---
+++ Oxygen_test2/application/views/login/login_fail.php
@@ -0,0 +1,26 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Login Page</title>
+        <link rel="shortcut icon" href="<?php echo base_url(); ?>CSS/images/favicon.ico"/>
+        <link href="<?php echo base_url(); ?>CSS/login.css" rel="stylesheet" type="text/css" media="screen" />
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="login">
+                <h1>&CircleTimes;User Login Failed&CircleTimes;</h1>
+                <div>
+                    <p>Your information is not correct, please try <a href="<?php echo base_url();?>index.php/login/index/">again</a>!</p>
+                </div>
+            </div>
+
+            <div id="home">
+                <a href="<?php echo base_url(); ?>index.php/home/index/"><img src="<?php echo base_url(); ?>CSS/images/background/home_button.png"/></a>
+            </div>
+
+            <?php $this->load->view('register/register_footer'); ?>
