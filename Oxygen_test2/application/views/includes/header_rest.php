88d1d8c97900d03ed9c6ef7df1d0e0f76a2edee2
arian.liu@gmail.com <arian.liu@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-10 11:08:28 +0000

    Added Facebook and Twitter 'Like' Buttons
    Change footer CSS to margin-top:60px

--- Oxygen_test2/application/views/includes/header_rest.php
+++ Oxygen_test2/application/views/includes/header_rest.php
@@ -368,9 +368,11 @@ function textCounter( field, countfield, maxlimit ) {
                         <li><a href="<?php echo base_url();?>index.php/home/activity_page/"><img src="<?php echo base_url();?>CSS/images/background/activity.png"></a></li>
                         <li><a href="<?php echo base_url();?>index.php/home/test_resilience/"><img src="<?php echo base_url();?>CSS/images/background/resilience.png"></a></li>
                         <li><a href="<?php echo base_url();?>index.php/home/portfolio_coa_motto/#MS"><img src="<?php echo base_url();?>CSS/images/background/coat_of_arm.png"></a></li>
-                        <li><a href="<?php echo base_url();?>index.php/home/portfolio_coa_motto/#MS"><img src="<?php echo base_url();?>CSS/images/background/portfolio.png"></a></li>                        
+                        <li><a href="<?php echo base_url();?>index.php/home/portfolio_coa_motto/#MS"><img src="<?php echo base_url();?>CSS/images/background/portfolio.png"></a></li>
                         <!--<li><a href="http://sit.rp.edu.sg/91234/assignment2/images/StartingSnow.swf"><img>Game</a></li>-->
                     </ul>
                 </div>
             </div>
+
+           
             <!--end #header -->
