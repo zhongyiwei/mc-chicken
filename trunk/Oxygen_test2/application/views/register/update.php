c1aad5438880a542781fefdc3283fcdc30ddc83a
tom.caibowen@gmail.com <tom.caibowen@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-11 06:59:11 +0000

    update seeker info start

--- trunk/Oxygen_test2/application/views/register/update.php
+++ trunk/Oxygen_test2/application/views/register/update.php
@@ -2,71 +2,78 @@
 <?php $this->load->view('register/register_header'); ?>
 
 <?php $this->load->view('includes/banner_general'); ?>
-<h1>Update User Info</h1>
-
-<table cellpadding="5" cellspace="5" style="text-align: left;">
-    <?php echo form_open('update_info/info'); ?>
-
-    <tr>
-        <th>Name:</th>
-        <td><?php echo form_input('name', set_value('name',$name), 'id="name"'); ?></td>
-        <td><?php echo form_error('name'); ?></td>
-    </tr>
-
-    <tr>
-        <th>Gender:</th>
-        <td><?php
-    $options = array(
-        'Male' => 'Male',
-        'Female' => 'Female',
-    );
-    echo form_dropdown('gender', $options, set_value('gender',$gender));
-    ?></td>
-        <td><?php echo form_error('gender'); ?></td>
-    </tr>
-
-    <tr>
-        <th>Date Of Birth:</th>
-        <td><?php echo form_input('date_of_birth', set_value('date_of_birth',$dob), 'id="datepicker123"'); ?></td>
-        <td><?php echo form_error('date_of_birth'); ?></td>
-    </tr>
+<div>
 
 
-    <tr>
-        <th>Nationality:</th>
-        <td><?php
-    $options = array(
-        'Burma' => 'Burma',
-        'China' => 'China',
-        'India' => 'India',
-        'Indonesia' => 'Indonesia',
-        'Malaysia' => 'Malaysia',
-        'Singaporean' => 'Singaporean',
-        'Thailand' => 'Thailand',
-        'Vietnam' => 'Vietnam',
-        'Others' => 'Others'
-    );
-    echo form_dropdown('nationality', $options, set_value('nationality',$nationality));
-    ?></td>
-        <td><?php echo form_error('nationality'); ?></td>
-    </tr>
+    <h1>Update User Info</h1>
 
-    <tr>
-        <th>Mobile Number:</th>
-        <td><?php echo form_input('mobile_number', set_value('mobile_number',$hp)); ?></td>
-        <td><?php echo form_error('mobile_number'); ?></td>
-    </tr>
+    <table cellpadding="5" cellspace="5" style="text-align: left;">
+        <?php echo form_open('update_info/info'); ?>
 
-    <tr>
-        <th>Email:</th>
-        <td><?php echo form_input('email', set_value('email',$email)); ?></td>
-        <td><?php echo form_error('email'); ?></td>
-    </tr>
+        <tr>
+            <th>Name:</th>
+            <td><?php echo form_input('name', set_value('name', $name), 'id="name"'); ?></td>
+            <td><?php echo form_error('name'); ?></td>
+        </tr>
 
-    <tr>
-        <td><?php echo form_submit('submit', 'Submit'); ?></td>
-    </tr>
-    <?php echo form_close(); ?>
-</table>
+        <tr>
+            <th>Gender:</th>
+            <td><?php
+        $options = array(
+            'Male' => 'Male',
+            'Female' => 'Female',
+        );
+        echo form_dropdown('gender', $options, set_value('gender', $gender));
+        ?></td>
+            <td><?php echo form_error('gender'); ?></td>
+        </tr>
 
-<?php $this->load->view('includes/footer_general'); ?>
+        <tr>
+            <th>Date Of Birth:</th>
+            <td><?php echo form_input('date_of_birth', set_value('date_of_birth', $dob), 'id="datepicker123"'); ?></td>
+            <td><?php echo form_error('date_of_birth'); ?></td>
+        </tr>
+
+
+        <tr>
+            <th>Nationality:</th>
+            <td><?php
+                $options = array(
+                    'Burma' => 'Burma',
+                    'China' => 'China',
+                    'India' => 'India',
+                    'Indonesia' => 'Indonesia',
+                    'Malaysia' => 'Malaysia',
+                    'Singaporean' => 'Singaporean',
+                    'Thailand' => 'Thailand',
+                    'Vietnam' => 'Vietnam',
+                    'Others' => 'Others'
+                );
+                echo form_dropdown('nationality', $options, set_value('nationality', $nationality));
+        ?></td>
+            <td><?php echo form_error('nationality'); ?></td>
+        </tr>
+
+        <tr>
+            <th>Mobile Number:</th>
+            <td><?php echo form_input('mobile_number', set_value('mobile_number', $hp)); ?></td>
+            <td><?php echo form_error('mobile_number'); ?></td>
+        </tr>
+
+        <tr>
+            <th>Email:</th>
+            <td><?php echo form_input('email', set_value('email', $email)); ?></td>
+            <td><?php echo form_error('email'); ?></td>
+        </tr>
+
+        <tr>
+            <td><?php echo form_submit('submit', 'Submit'); ?></td>
+        </tr>
+        <?php echo form_close(); ?>
+    </table>
+</div>
+
+<div id="home">
+    <a href="<?php echo base_url(); ?>index.php/home/index/"><img src="<?php echo base_url(); ?>CSS/images/background/home_button.png"/></a>
+</div>
+<?php $this->load->view('register/register_footer'); ?>
