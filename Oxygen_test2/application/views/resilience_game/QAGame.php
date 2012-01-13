3d126cb699d35a1156319b0099b75ffeba9af9e1
sippoujulian@gmail.com <sippoujulian@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-13 12:31:26 +0000

    Update of the Resillience Game

--- Oxygen_test2/application/views/resilience_game/QAGame.php
+++ Oxygen_test2/application/views/resilience_game/QAGame.php
@@ -1,3 +1,8 @@
+
+
+
+<?php $this->load->view('includes/header_general');?>
+
 <link href="<?php echo base_url(); ?>CSS/style_subpage_main.css" rel="stylesheet" type="text/css" media="screen" />
 <!--[if IE]><link href="<?php echo base_url(); ?>CSS/styleForIE.css" rel="stylesheet" type="text/css" /><![endif]-->
   <!--[if IE]><link href="<?php echo base_url(); ?>CSS/styleForIEfooter.css" rel="stylesheet" type="text/css" /><![endif]-->
@@ -9,11 +14,10 @@
 
         </ul>
     </div>
-    <div id="content_sub">
-        <div class="post" style="margin:0px; padding:0px;">
+    <div id="content_sub" style="height:auto">
+        <div class="post" >
             <h2 class="title">Resilience Game</h2>
-            <div class="entry" style="margin:50px auto; padding:0px">
-  
+            <div class="entry" style="margin-top:50px; margin-left:32x; padding:0px">
                     <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="630" height="315" id="Starting Snow" align="middle">
                         <param name="movie" value="../../flash/StartingSnow.swf" />
                         <param name="quality" value="high" />
@@ -53,7 +57,9 @@
 
         </div><!-- End of div class entry -->
     </div><!-- End of div class post -->
-
     <!-- end #content -->
-
-
+</div>
+    <div id="footer" align="center">
+    <div id="image_footer"><img id="opener" onclick="loadFlow()" src="<?php echo base_url();?>CSS/images/background/what_is_now.png" alt="" /></img></div>
+    <div id="image_footer"><a href="<?php echo base_url();?>index.php/home/why_ms/#WMS"><img src="<?php echo base_url();?>CSS/images/background/do_u_know.png" alt="" /></a></div>
+    <?php $this->load->view('includes/footer_general');?>
