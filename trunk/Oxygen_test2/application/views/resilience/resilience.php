df4b5467e4a08005de06c9fd529f06259f64729f
sippoujulian@gmail.com <sippoujulian@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-13 12:33:54 +0000

    Add top banner link to the resilience Game page

--- trunk/Oxygen_test2/application/views/resilience/resilience.php
+++ trunk/Oxygen_test2/application/views/resilience/resilience.php
@@ -1,29 +1,29 @@
-<link href="<?php echo base_url();?>CSS/style_subpage_main.css" rel="stylesheet" type="text/css" media="screen" />
-<!--[if IE]><link href="<?php echo base_url();?>CSS/styleForIE.css" rel="stylesheet" type="text/css" /><![endif]-->
-  <!--[if IE]><link href="<?php echo base_url();?>CSS/styleForIEfooter.css" rel="stylesheet" type="text/css" /><![endif]-->
+<link href="<?php echo base_url(); ?>CSS/style_subpage_main.css" rel="stylesheet" type="text/css" media="screen" />
+<!--[if IE]><link href="<?php echo base_url(); ?>CSS/styleForIE.css" rel="stylesheet" type="text/css" /><![endif]-->
+  <!--[if IE]><link href="<?php echo base_url(); ?>CSS/styleForIEfooter.css" rel="stylesheet" type="text/css" /><![endif]-->
 
 <div id="page">
-         <div id="sub-nav">
+    <div id="sub-nav">
         <ul>
             <li><a href="<?php echo base_url(); ?>index.php/home/resilience_test/gameproto.html" >Resilience Test</a></li>
-          <li><a href="<?php echo base_url(); ?>index.php/home/QAGamePortol" >Resilience Game</a></li>
+            <li><a href="<?php echo base_url(); ?>index.php/home/QAGamePortol" >Resilience Game</a></li>
 
         </ul>
-         </div>
+    </div>
     <div id="content_sub">
         <div class="post">
             <h2 class="title">Resilience Module</h2>
             <div class="entry">
-			<div id="popup">
-			Resilience Do you know
-			</div>
-	</div>
+                <div id="popup">
+                    Resilience Do you know
+                </div>
+            </div>
 
 
 
-            </div><!-- End of div class entry -->
-        </div><!-- End of div class post -->
+        </div><!-- End of div class entry -->
+    </div><!-- End of div class post -->
 
-<!-- end #content -->
+    <!-- end #content -->
 
 
