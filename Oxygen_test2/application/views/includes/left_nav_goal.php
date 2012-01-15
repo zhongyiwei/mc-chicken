5af5f0cc819e51815b125e17c6d020708868617e
tom.caibowen@gmail.com <tom.caibowen@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-15 06:40:50 +0000

    goal css

--- Oxygen_test2/application/views/includes/left_nav_goal.php
+++ Oxygen_test2/application/views/includes/left_nav_goal.php
@@ -1,47 +1,5 @@
 <div id="xxx">
-    <ul class="container" style="display:none">
-        <li class="menu">
-
-            <ul>
-                <li class="button"><a href="#" class="green">Do You Know... <span></span></a></li>
-
-                <li class="dropdown">
-                    <ul>
-                        <li><a href="<?php echo base_url();?>index.php/home/goal/#H0" >Why Set Goal?</a></li>
-                        <li><a href="<?php echo base_url();?>index.php/home/goal/#H1" >Successful Example</a></li>
-                        <li><a href="<?php echo base_url();?>index.php/home/goal/#H2" >Be SMART</a></li>
-                        <li><a href="<?php echo base_url();?>index.php/home/goal/#H3">Know Holistic Goals</a></li>
-                        <li><a href="<?php echo base_url();?>index.php/home/goal/">Why Set Activity?</a></li>
-                        <li><a href="<?php echo base_url();?>index.php/home/goal/">How To Set Activity?</a></li>
-
-                    </ul>
-                </li>
-
-            </ul>
-
-        </li>
-
-        <li class="menu">
-
-            <ul>
-                <li class="button"><a href="#" class="orange">What To Do... <span></span></a></li>
-
-                <li class="dropdown">
-                    <ul>
-                        <li><a href="<?php echo base_url();?>index.php/home/holistic/">Set Your Goals</a></li>
-                        <li><a href="<?php echo base_url();?>index.php/home/activity/">Set Activity</a></li>
-                        <li><a href="<?php echo base_url();?>index.php/home/set_reminder/">Set Reminder</a></li>
-                        <li><a href="<?php echo base_url();?>index.php/home/see_goal/">View Your Goals</a></li>
-                        <li><a href="<?php echo base_url();?>index.php/home/activity_list/">View Your Activities</a></li>
-                        <li><a href="<?php echo base_url();?>index.php/home/activity_tracking/">Track Your Activities</a></li>
-                        <!--<li><a href="<?php echo base_url();?>index.php/home/reminder_setting/">Your Reminder Setting</a></li>-->
-                    </ul>
-                </li>
-
-            </ul>
-
-        </li>
-    </ul></div>
+</div>
 <!-- end #sidebar -->
 <div style="clear: both;">&nbsp;</div>
 </div>
