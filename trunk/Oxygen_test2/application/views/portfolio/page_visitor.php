269f956b4ba0bb6dcd910e4e0fa7026f048e2b1d
tom.caibowen@gmail.com <tom.caibowen@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-15 07:10:29 +0000

    portfolio and coat of arm, login error page done

--- trunk/Oxygen_test2/application/views/portfolio/page_visitor.php
+++ trunk/Oxygen_test2/application/views/portfolio/page_visitor.php
@@ -1,8 +1,16 @@
-<?php $this->load->view('includes/header_general');?>
-<?php $this->load->view('includes/banner_general');?>
+<?php $this->load->view('register/register_header'); ?>
+<div id="register">
+    <h1 style="padding-top: 20px;">&otimes;Error&otimes;</h1>
+    
+    <p style="padding-left: 20px;font-family:arial;color:green;font-size:16px">You have not login, please <a href="<?php echo base_url();?>index.php/login/index/">login</a> first!</p>
+    <p style="padding-left: 20px;font-family:arial;color:green;font-size:12px">My Portfolio is only displayed to the Members. If you are not a member yet, please <a href="<?php echo base_url();?>index.php/login/register">Sign up</a> first!</p>
+</div>
 
-<h1>&otimes;Error&otimes;</h1>
 
-<p style="font-family:arial;color:green;font-size:15px">You have not login, please <a href="<?php echo base_url();?>index.php/home/index/">login</a> first!</p>
-<p style="font-family:arial;color:green;font-size:12px">My Portfolio is only displayed to the Members. If you are not a member yet, please <a href="<?php echo base_url();?>index.php/login/register">Sign up</a> first!</p>
-<?php $this->load->view('includes/footer_general'); ?>
+
+
+<div id="home">
+    <a href="<?php echo base_url();?>index.php/home/index/"><img src="<?php echo base_url();?>CSS/images/background/home_button.png"/></a>
+</div>
+
+<?php $this->load->view('register/register_footer'); ?>
