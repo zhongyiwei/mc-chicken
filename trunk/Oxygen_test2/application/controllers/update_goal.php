eef88b760a0271e76688f4214d28dbce4ac71672
tom.caibowen@gmail.com <tom.caibowen@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-16 13:11:13 +0000

    update goal done

--- trunk/Oxygen_test2/application/controllers/update_goal.php
+++ trunk/Oxygen_test2/application/controllers/update_goal.php
@@ -8,6 +8,8 @@ class Update_goal extends CI_Controller{
         $this->load->model('goal_setting');
         $query=$this->goal_setting->update_goal();
         if($query){
+            $this->load->view('subpage/update_ok');
+            $this->output->set_header('refresh:2; url='.base_url().'index.php/home/holistic/');
             redirect('home/holistic');
         }elseif($query==NULL){
             $this->load->view('subpage/update_goal_error');
--- trunk/Oxygen_test2/application/views/subpage/wrong_update.php
+++ trunk/Oxygen_test2/application/views/subpage/wrong_update.php
@@ -1,8 +1,11 @@
-<?php $this->load->view('includes/header_general');?>
-<?php $this->load->view('includes/banner_general');?>
+<?php $this->load->view('register/register_header'); ?>
+<div id="register">
+    <h1>&otimes;Error&otimes;</h1>
 
-<h1>&otimes;Error&otimes;</h1>
+    <p>There is a wrong about updating your goal, please <a href="<?php echo base_url();?>index.php/home/holistic/">try again</a>!</p>
+</div>
 
-<p>There is a wrong about updating your goal, please <a href="<?php echo base_url();?>index.php/home/holistic/">try again</a>!</p>
-
+<div id="home">
+    <a href="<?php echo base_url();?>index.php/home/index/"><img src="<?php echo base_url();?>CSS/images/background/home_button.png"/></a>
+</div>
 <?php $this->load->view('includes/footer_general'); ?>
