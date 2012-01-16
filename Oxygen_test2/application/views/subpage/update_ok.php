8edc978984404b19f429dbdf8155225463a16d99
tom.caibowen@gmail.com <tom.caibowen@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-16 13:11:13 +0000

    update goal done

--- Oxygen_test2/application/views/subpage/update_ok.php
+++ Oxygen_test2/application/views/subpage/update_ok.php
@@ -1,9 +1,12 @@
 <?php $this->load->view('register/register_header'); ?>
 <div id="register">
-    <h1>&otimes;Error&otimes;</h1>
-
-    <p>Some of your activities have not been completed, please try to finish them before you update your goal status. Please <a href="<?php echo base_url();?>index.php/home/activity_list/"> View Activity</a></p>
+    <h1>Update Successful</h1>
+    <div styel="margin-left:50px; margin-top:50px;color:black;">
+        <p>The system is directing you to <strong>Goal Setting Page</strong>!</p>
+        <p>Please wait for a while...</p>
+    </div>
 </div>
+
 <div id="home">
     <a href="<?php echo base_url();?>index.php/home/index/"><img src="<?php echo base_url();?>CSS/images/background/home_button.png"/></a>
 </div>
