dc8af0305c5867efe57f5cc5cd05aafbea91dcdd
sippoujulian@gmail.com <sippoujulian@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-16 14:22:54 +0000

    Add Instructions for resilience_game/QAGame.php Fixed Position Problem in the Flash Game Fixed the Problem that mission statement not able to use.

--- Oxygen_test2/application/views/resilience_game/QAGame.php
+++ Oxygen_test2/application/views/resilience_game/QAGame.php
@@ -14,11 +14,11 @@
             <li><a href="<?php echo base_url(); ?>index.php/home/QAGamePortol" >Resilience Game</a></li>
         </ul>
     </div>
-    <div id="content_sub" style="height:auto">
-        <div class="post" >
-            <h2 class="title">Resilience Game</h2>
-            <div class="entry" style="margin-top:50px; margin-left:32px; padding:0px">
-                <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="630" height="315" id="Starting Snow" align="middle">
+    <div id="content_sub" style="height:auto;">
+        <div class="post" style="margin-left:20px; overflow:hidden;" >
+            <h2 class="title" style=" margin-left: -7px; overflow: hidden; width:640px;">Resilience Game</h2>
+            <div class="entry" style="margin-top:20px; margin-left:-80px; padding:0px;">
+                <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="800" height="400" id="Starting Snow" align="middle">
                     <param name="movie" value="../../flash/StartingSnow.swf" />
                     <param name="quality" value="high" />
                     <param name="bgcolor" value="#ffffff" />
@@ -31,7 +31,7 @@
                     <param name="salign" value="" />
                     <param name="allowScriptAccess" value="sameDomain" />
                     <!--[if !IE]>-->
-                    <object type="application/x-shockwave-flash" data="../../flash/StartingSnow.swf" width="630" height="315">
+                    <object type="application/x-shockwave-flash" data="../../flash/StartingSnow.swf" width="800" height="400">
                         <param name="movie" value="../../flash/StartingSnow.swf" />
                         <param name="quality" value="high" />
                         <param name="bgcolor" value="#ffffff" />
@@ -51,6 +51,27 @@
                     </object>
                     <!--<![endif]-->
                 </object>
+            </div>
+            <div style="background:rgba(0,0,0,0.6);color:white;">
+                <table width="700" border="1" >
+                    <tr >
+                        <td style="vertical-align: middle;"> <img src="<?php echo base_url(); ?>CSS/images/instruction1.png" alt="Press Up to Jump" /></td>
+                        <td style="vertical-align: middle;">Jump</td>
+                        <td style="vertical-align: middle;"> <img src="<?php echo base_url(); ?>CSS/images/instruction2.png" alt="Press Right to Walk"/> </td>
+                        <td style="vertical-align: middle; 	
+                            border-right-width: thin;
+                            border-right-style: solid;
+                            border-right-color: #FFF;">Walk</td>
+                        <td style="vertical-align: middle;"><img src="<?php echo base_url(); ?>CSS/images/wing.png" alt="When Flying Walk" width="48px" height="48px;" style=" margin-left: 20px;"/> </td>
+                        <td style="vertical-align: middle;">When Flying</td>
+                        <td style="vertical-align: middle;"> <img src="<?php echo base_url(); ?>CSS/images/instruction1.png" alt="Press Up to Fly Higher" /></td>
+                        <td style="vertical-align: middle;">Fly Higher</td>
+                        <td style="vertical-align: middle;"> <img src="<?php echo base_url(); ?>CSS/images/instruction3.png" alt="Press Up to Fly Higher" /></td>
+                        <td style="vertical-align: middle;">Fly Lower</td>
+                    </tr>
+                </table>
+
+
             </div>
 
 
