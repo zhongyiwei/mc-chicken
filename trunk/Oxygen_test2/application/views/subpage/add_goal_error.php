94eba4130fedade61e55369540917d62a8e1a5fd
tom.caibowen@gmail.com <tom.caibowen@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-20 10:47:22 +0000

    goal update, it only allows the people to setup 3 goals

--- trunk/Oxygen_test2/application/views/subpage/add_goal_error.php
+++ trunk/Oxygen_test2/application/views/subpage/add_goal_error.php
@@ -2,7 +2,7 @@
 <div id="register">
     <h1>&otimes;Error&otimes;</h1>
 
-    <p>You have set your goal already. And you current goal does not finish, you can not insert another goal! Please insert <a href="<?php echo base_url(); ?>index.php/home/holistic/">again</a>!</p>
+    <p>The number of your active goals are up to 3. Please finish the rest goals first before you insert another goal! Click to <a href="<?php echo base_url(); ?>index.php/home/see_goal/">View your Goals</a>!</p>
 </div>
 <div id="home">
     <a href="<?php echo base_url(); ?>index.php/home/index/"><img src="<?php echo base_url(); ?>CSS/images/background/home_button.png"/></a>
