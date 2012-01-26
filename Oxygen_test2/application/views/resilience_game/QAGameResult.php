575942092efaaf7251772d62b3b5504d39fb4968
sippoujulian@gmail.com <sippoujulian@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-26 10:05:30 +0000

    Modify the QAGameResult.php

--- Oxygen_test2/application/views/resilience_game/QAGameResult.php
+++ Oxygen_test2/application/views/resilience_game/QAGameResult.php
@@ -3,6 +3,7 @@ session_start();
 $gameResult = $_POST['score'];
 $seeker_id=$this->session->userdata('seeker_id');
 
-$this->db->query("INSERT INTO resilience_game_result ('seeker_id','score')VALUES('$seeker_id','$gameResult')");
+
+$this->db->query("INSERT INTO `resilience_game_result` (`seeker_id`, `score`) VALUES ('$seeker_id','$gameResult')");
 
 ?>
