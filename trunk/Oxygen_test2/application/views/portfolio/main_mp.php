d3cdf0c2b11cfab6bb793cbc41f77c462267dbfd
tom.caibowen@gmail.com <tom.caibowen@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-26 10:19:02 +0000

    color code login

--- trunk/Oxygen_test2/application/views/portfolio/main_mp.php
+++ trunk/Oxygen_test2/application/views/portfolio/main_mp.php
@@ -1,4 +1,9 @@
+<?php if($this->session->userdata('type')=='negative'){ ?>
+<link href="<?php echo base_url();?>CSS/style_subpage_main_neutral.css" rel="stylesheet" type="text/css" media="screen" />
+<?php }else{?>
 <link href="<?php echo base_url();?>CSS/style_subpage_main.css" rel="stylesheet" type="text/css" media="screen" />
+<?php }?>
+
 <link href="<?php echo base_url();?>CSS/coa_design.css" rel="stylesheet" type="text/css" media="screen" />
 <!--[if IE]><link href="<?php echo base_url();?>CSS/styleForIE.css" rel="stylesheet" type="text/css" /><![endif]-->
   <!--[if IE]><link href="<?php echo base_url();?>CSS/styleForIEfooter.css" rel="stylesheet" type="text/css" /><![endif]-->
--- trunk/Oxygen_test2/application/views/subpage/update_goal_setting.php
+++ trunk/Oxygen_test2/application/views/subpage/update_goal_setting.php
@@ -1,8 +1,13 @@
 <?php $this->load->view('includes/header_general'); ?>
 
 <?php $this->load->view('includes/banner_general'); ?>
+<?php if($this->session->userdata('type')=='negative'){ ?>
+<link href="<?php echo base_url();?>CSS/style_subpage_main_neutral.css" rel="stylesheet" type="text/css" media="screen" />
+<?php }else{?>
 <link href="<?php echo base_url();?>CSS/style_subpage_main.css" rel="stylesheet" type="text/css" media="screen" />
-  <!--[if IE]><link href="<?php echo base_url();?>CSS/styleForIE.css" rel="stylesheet" type="text/css" /><![endif]-->
+<?php }?>
+
+<!--[if IE]><link href="<?php echo base_url();?>CSS/styleForIE.css" rel="stylesheet" type="text/css" /><![endif]-->
   <!--[if IE]><link href="<?php echo base_url();?>CSS/styleForIEfooter.css" rel="stylesheet" type="text/css" /><![endif]-->
 
 <div id="page">
