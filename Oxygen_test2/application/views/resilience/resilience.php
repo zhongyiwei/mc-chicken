4f8dd7e841ddb8ca44bc3c334cfc8fbb27bbb31b
tom.caibowen@gmail.com <tom.caibowen@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-01-26 10:19:02 +0000

    color code login

--- Oxygen_test2/application/views/resilience/resilience.php
+++ Oxygen_test2/application/views/resilience/resilience.php
@@ -1,4 +1,9 @@
-<link href="<?php echo base_url(); ?>CSS/style_subpage_main.css" rel="stylesheet" type="text/css" media="screen" />
+<?php if($this->session->userdata('type')=='negative'){ ?>
+<link href="<?php echo base_url();?>CSS/style_subpage_main_neutral.css" rel="stylesheet" type="text/css" media="screen" />
+<?php }else{?>
+<link href="<?php echo base_url();?>CSS/style_subpage_main.css" rel="stylesheet" type="text/css" media="screen" />
+<?php }?>
+
 <!--[if IE]><link href="<?php echo base_url(); ?>CSS/styleForIE.css" rel="stylesheet" type="text/css" /><![endif]-->
   <!--[if IE]><link href="<?php echo base_url(); ?>CSS/styleForIEfooter.css" rel="stylesheet" type="text/css" /><![endif]-->
 
