b44e8d70048c7981119c5c4bbfec2b2435271cc0
arian.liu@gmail.com <arian.liu@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-02-07 10:44:09 +0000

    -Added Archived Activity to view completed activities -Modify activity tracking so that only active activities are displayed

--- trunk/Oxygen_test2/application/views/set_activity/main_al.php
+++ trunk/Oxygen_test2/application/views/set_activity/main_al.php
@@ -31,7 +31,8 @@ $id=$this->session->userdata('seeker_id');
           
             
             <div class="entry" width="">  <div id="popup">
-                    <p style="font-size:150%; color:black;" >View all your records below</p> 
+                    <a href="<?php echo base_url(); ?>index.php/home/archived_activity/"><p style="font-size:150%; color:blue; text-align:right" >Archived Activities</p></a>
+                    <p style="font-size:150%; color:black;" >View all your active activities below</p>
                 <p style="font-family:arial;color:green;font-size:16px">Please click the activity name to update it</p>
                 
                 <table id="myTable" style="table-layout:fixed; width: 600px" class="tablesorter">
