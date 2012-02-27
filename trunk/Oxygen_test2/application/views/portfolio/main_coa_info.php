128104e682af86299663427e22506cbd0d5d3973
qianhua1009@gmail.com <qianhua1009@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-02-27 19:31:07 +0000

    change write up

--- trunk/Oxygen_test2/application/views/portfolio/main_coa_info.php
+++ trunk/Oxygen_test2/application/views/portfolio/main_coa_info.php
@@ -11,19 +11,43 @@
     <div id="content_sub">
 
          <div class="post">
-<h2 class="title" id="VALUE">Your Coat of Arms</h2>
+<h2 class="title" id="VALUE">Coat of Arms</h2>
             <div class="entry">
                 <img src="<?php echo base_url();?>web_images/shield of arm/Coat_of_Arms2.png" alt="Coat of Arm"  id="Coat_of_Arms1"/>
                 <img src="<?php echo base_url();?>web_images/shield of arm/Coat_of_Arms4.png" alt="Coat of Arm"  id="Coat_of_Arms1"/>
-                <h2>The Coat of Arms: </h2>
-                <p style="font-family:arial;color:black;font-size:14px">A coat of arms is a distinctive heraldic design on a tunic used to cover and protect armor, but the term is more broadly applied to mean a full heraldic
+                <p>
+                    A coat of arms is a distinctive heraldic design on a tunic used to cover and protect armor, but the term is more broadly applied to mean a full heraldic
                     achievement which consists of a shield and certain accessories. In either sense, the design is a symbol unique to a person, family, corporation, or state.
                     Such displays are also commonly called armorial bearings, armorial devices, heraldic devices, or arms.
                 <br>
-                Your personal coat of arms will be a design which can reflect your values and motto of your life. The 4 symbols on the shield represent each of your values.
+                Your personal coat of arms will be a design which can reflect your values. The 4 symbols on the shield represent each of your values.
                 Your personal motto will be on the manner. Your personal Coat of Arms as a whole will be a unique symbol of you. It represents what a person you are.
                 </p>
             </div>
+
+<h2 class="title" id="VALUE">Why to set your Coat of Arms?</h2>
+            <div class="entry">
+                <p>
+                    After identifying your personal mission statement, and values, you know that you are unique in life, you have a purpose in life and you represent something
+                    so unique that only you have. You are who you are! Your coat of arms is a symbol unique to you.  The four images on your coat of arms are your values.
+                    It is symbolic to you and your value system.    
+                </p>
+            </div>
+
+<h2 class="title" id="VALUE">Set Coat of Arms on Oxygen life guidance: </h2>
+            <div class="entry">
+                <h2>Set Coat of Arms</h2><br><p>
+                    You can set your Coat of Arms on the "Coat of Arms" page by clicking the "Coat of Arms" button on the navigation 
+                    bar on the left. The four images on your coat of arms represent the 4 values you set earlier.  You can freely choose
+                    the design of the shield based on your preference.  Your own Coat of Arms will be saved in your account after you submit.
+                    After setting your Coat of Arms, you can also right click the Coat of Arms symbol on the screen and save the image to your local computer.
+                </p>
+                <h2>Modify Coat of Arms</h2><br><p>
+                   You can modify and update the Coat of Arms you previously set by clicking the "Modify COA" button on the navigation bar
+                   on the top of the "Coat of Arms" page.
+                </p>
+            </div>
+
          </div>
     </div>
     <!-- end #content -->
