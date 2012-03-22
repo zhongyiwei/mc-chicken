389bc0a9dea12a654e731d799d5fa4e40ba718a3
sippoujulian@gmail.com <sippoujulian@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-03-22 03:55:02 +0000

    Update the portfolio page

--- Oxygen_test2/application/views/portfolio/main_portfolio.php
+++ Oxygen_test2/application/views/portfolio/main_portfolio.php
@@ -12,7 +12,7 @@
 <div id = "bodycolor"></div>
 <div id="popupContent" >
     <div onclick="disablePopup()" style="cursor:pointer; margin-left:670px;font-size:24px;border-width: 3px;border-color:#999;border-style: solid;text-align: center;padding-bottom: 2px;width:25px;">x</div>
-    <p class="askforsendemail">According to the research by the Dominican University of California, those who commit their goals by sharing them with his friends are more likely to achieve their goals. Do you want to send your achievement to the friends you refer to when you register?</p>
+    <p class="askforsendemail">According to the research by the Dominican University of California, those who commit their goals by sharing them with friends are more likely to achieve their goals. Do you want to notify your friend whom you nominated as your mentor?</p><p>No mentor? <a href="<?php echo base_url(); ?>index.php/home/personal_info/">Click here</a></p>
     <a href="<?php echo base_url(); ?>index.php/home/email_pdf" class="yesButton">Yes</a>
     <a class="noButton" href="<?php echo base_url(); ?>index.php/home/portfolio_export_pdf?id=<?php echo $this->session->userdata('seeker_id') ?>">No</a>
     <p id="ContentArea"> </p>
