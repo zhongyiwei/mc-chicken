cc308a45f90b7fa6ecad212c387ffc033c859b1b
qianhua1009@gmail.com <qianhua1009@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-04-12 15:34:00 +0000

    change in set reminder, mission, portfolio, coa, for IE

--- trunk/Oxygen_test2/application/views/mission_statement/main_ums.php
+++ trunk/Oxygen_test2/application/views/mission_statement/main_ums.php
@@ -3,6 +3,8 @@
 <?php }else{?>
 <link href="<?php echo base_url();?>CSS/style_subpage_main.css" rel="stylesheet" type="text/css" media="screen" />
 <?php }?>
+
+<link href="<?php echo base_url();?>CSS/coa_design.css" rel="stylesheet" type="text/css" media="screen" />
 <!--[if IE]><link href="<?php echo base_url();?>CSS/styleForIE.css" rel="stylesheet" type="text/css" /><![endif]-->
   <!--[if IE]><link href="<?php echo base_url();?>CSS/styleForIEfooter.css" rel="stylesheet" type="text/css" /><![endif]-->
 <div id="page">
