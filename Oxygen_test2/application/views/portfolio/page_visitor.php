7cb5614e0a763c11a89850078b465ae23adb4ca6
tom.caibowen@gmail.com <tom.caibowen@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-04-17 03:00:24 +0000

    portfolio/page_visitor.php error page font done

--- Oxygen_test2/application/views/portfolio/page_visitor.php
+++ Oxygen_test2/application/views/portfolio/page_visitor.php
@@ -1,9 +1,11 @@
 <?php $this->load->view('register/register_header'); ?>
 <div id="register">
     <h1 style="padding-top: 20px;">&otimes;Error&otimes;</h1>
-
-    <p style="padding-left: 20px;font-family:arial;color:green;font-size:16px">You have not login, please <a href="<?php echo base_url(); ?>index.php/login/index/">login</a> first!</p>
-    <p style="padding-left: 20px;font-family:arial;color:green;font-size:12px">My Portfolio is only displayed to the Members. If you are not a member yet, please <a href="<?php echo base_url(); ?>index.php/login/register">Sign up</a> first!</p>
+    <div id="p_font">
+        <h2 style="color: grey;">You have not login, please <a href="<?php echo base_url(); ?>index.php/login/index/">login</a> first!</h2>
+        <h2 style="color: grey;">My Portfolio is only displayed to the Members. If you are not a member yet, please <a href="<?php echo base_url(); ?>index.php/login/register">Sign up</a> first!</h2>
+    </div>
+    
 </div>
 
 
