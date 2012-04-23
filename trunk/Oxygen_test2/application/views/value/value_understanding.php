c1f0baa5fe1c4bb3081b9d6ecaae1d93775532de
tom.caibowen@gmail.com <tom.caibowen@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-04-23 09:35:23 +0000

    add the new video

--- trunk/Oxygen_test2/application/views/value/value_understanding.php
+++ trunk/Oxygen_test2/application/views/value/value_understanding.php
@@ -1,33 +1,33 @@
-<?php if($this->session->userdata('type')=='negative'){ ?>
-<link href="<?php echo base_url();?>CSS/style_subpage_main_neutral.css" rel="stylesheet" type="text/css" media="screen" />
-<?php }else{?>
-<link href="<?php echo base_url();?>CSS/style_subpage_main.css" rel="stylesheet" type="text/css" media="screen" />
-<?php }?>
-<!--[if IE]><link href="<?php echo base_url();?>CSS/styleForIE.css" rel="stylesheet" type="text/css" /><![endif]-->
-       <!--[if IE]><link href="<?php echo base_url();?>CSS/styleForIEfooter.css" rel="stylesheet" type="text/css" /><![endif]-->
- 
+<?php if ($this->session->userdata('type') == 'negative') { ?>
+    <link href="<?php echo base_url(); ?>CSS/style_subpage_main_neutral.css" rel="stylesheet" type="text/css" media="screen" />
+<?php } else { ?>
+    <link href="<?php echo base_url(); ?>CSS/style_subpage_main.css" rel="stylesheet" type="text/css" media="screen" />
+<?php } ?>
+<!--[if IE]><link href="<?php echo base_url(); ?>CSS/styleForIE.css" rel="stylesheet" type="text/css" /><![endif]-->
+       <!--[if IE]><link href="<?php echo base_url(); ?>CSS/styleForIEfooter.css" rel="stylesheet" type="text/css" /><![endif]-->
+
 <div id="page">
-         <div id="sub-nav">
+    <div id="sub-nav">
         <ul>
             <li><a href="<?php echo base_url(); ?>index.php/home/determine_values/" >Set Values</a></li>
 
         </ul>
-         </div>
+    </div>
     <div id="content_sub">
-     	<div class="post">
+        <div class="post">
             <h2 class="title">Learn More About Values</h2>
             <div class="entry">
-<p><iframe width="560" height="350" src="http://www.youtube.com/embed/axMpeZjmoAg" frameborder="0" allowfullscreen></iframe></p>
-<br/><br/>
-</div>
-</div>
-             	<div class="post">
+                <p><iframe width="560" height="450" src="http://www.youtube.com/embed/axMpeZjmoAg" frameborder="0" allowfullscreen></iframe></iframe></p>
+                <br/><br/>
+            </div>
+        </div>
+        <div class="post">
             <h2 class="title">Learn More About Using Oxygen To Set Values</h2>
             <div class="entry">
-<p><iframe width="560" height="450" src="http://www.youtube.com/embed/CtpN1VYLeyA" frameborder="0" allowfullscreen></iframe></p>
-<br/><br/>
-</div>
-</div>  
-	
-<!-- end #content -->
-</div>
+                <p><iframe width="560" height="450" src="http://www.youtube.com/embed/9E7xBPrtGoA" frameborder="0" allowfullscreen></iframe></p>
+                <br/><br/>
+            </div>
+        </div>  
+
+        <!-- end #content -->
+    </div>
