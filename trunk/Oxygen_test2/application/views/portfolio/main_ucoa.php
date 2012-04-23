20f5526a9d634d09c93f1bf26f4cc81022cc7ab2
qianhua1009@gmail.com <qianhua1009@gmail.com@e892755d-ae8e-ed16-4ef6-a9aeaa6253e5>
2012-04-23 07:05:01 +0000

    change coat of arms selection to small images

--- trunk/Oxygen_test2/application/views/portfolio/main_ucoa.php
+++ trunk/Oxygen_test2/application/views/portfolio/main_ucoa.php
@@ -18,25 +18,25 @@
             <div class="entry">
 <?php //echo $shield?>
 <ul class="thumb">
-        <li><a onclick="ChgText('web_images/coa_image/shield/coa1.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa1.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa1.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa1.png"/></a>
+        <li><a onclick="ChgText('web_images/coa_image/shield/coa1.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa1.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa1_small.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa1.png"/></a>
 	</li>
-        <li><a onclick="ChgText('web_images/coa_image/shield/coa2.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa2.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa2.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa2.png"/></a>
+        <li><a onclick="ChgText('web_images/coa_image/shield/coa2.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa2.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa2_small.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa2.png"/></a>
 	</li>
-        <li><a onclick="ChgText('web_images/coa_image/shield/coa3.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa3.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa3.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa3.png"/></a>
+        <li><a onclick="ChgText('web_images/coa_image/shield/coa3.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa3.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa3_small.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa3.png"/></a>
 	</li>
-        <li><a onclick="ChgText('web_images/coa_image/shield/coa4.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa4.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa4.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa4.png"/></a>
+        <li><a onclick="ChgText('web_images/coa_image/shield/coa4.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa4.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa4_small.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa4.png"/></a>
 	</li>
-        <li><a onclick="ChgText('web_images/coa_image/shield/coa5.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa5.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa5.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa5.png"/></a>
+        <li><a onclick="ChgText('web_images/coa_image/shield/coa5.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa5.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa5_small.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa5.png"/></a>
 	</li>
-        <li><a onclick="ChgText('web_images/coa_image/shield/coa6.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa6.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa6.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa6.png"/></a>
+        <li><a onclick="ChgText('web_images/coa_image/shield/coa6.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa6.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa6_small.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa6.png"/></a>
 	</li>
-        <li><a onclick="ChgText('web_images/coa_image/shield/coa7.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa7.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa7.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa7.png"/></a>
+        <li><a onclick="ChgText('web_images/coa_image/shield/coa7.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa7.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa7_small.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa7.png"/></a>
 	</li>
-        <li><a onclick="ChgText('web_images/coa_image/shield/coa8.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa8.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa8.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa8.png"/></a>
+        <li><a onclick="ChgText('web_images/coa_image/shield/coa8.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa8.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa8_small.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa8.png"/></a>
 	</li>
-        <li><a onclick="ChgText('web_images/coa_image/shield/coa9.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa9.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa9.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa9.png"/></a>
+        <li><a onclick="ChgText('web_images/coa_image/shield/coa9.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa9.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa9_small.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa9.png"/></a>
 	</li>
-        <li><a onclick="ChgText('web_images/coa_image/shield/coa10.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa10.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa10.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa10.png"/></a>
+        <li><a onclick="ChgText('web_images/coa_image/shield/coa10.png');" href="<?php echo base_url();?>web_images/coa_image/shield/coa10.png"><img src="<?php echo base_url();?>web_images/coa_image/shield/coa10_small.png" alt="Click Me!" id="img_input" value="web_images/coa_image/shield/coa10.png"/></a>
 	</li>
 </ul>
 <?php $this->load->view('portfolio/ucoa_form');?>
@@ -44,7 +44,6 @@
             <h2 class="title" id="COA">Your Coat of Arms</h2>
             <div class="entry">
                 <?php $this->load->view('portfolio/coa_set');?>
-                <?php //$this->load->view('portfolio/portfolio_translate');?>
 <!--                <h4 align="center" style="color:purple;">The symbols in the center of the Coat of Arms represent your values: </h4>
                 <table border="1" align="center">
                         <th width="150px" align="center">Value 1: Top Left</th>
